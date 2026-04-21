44321e9eae4dd82128e6a534acf4ac9289cfeb7d
kjh <rlawngud1999@gmail.com>
2026-04-21 09:22:49 +0900

    [Feat][F][User_001] 라우터 guestOnly 범위 변경

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -105,15 +105,13 @@ const routes = [
     path: '/user',
     component: FirstLayout,
     redirect: { name: 'login' },
-    meta: {
-      guestOnly: true,
-    },
     children: [
       {
         path: 'login',
         name: 'login',
         component: () => import('@/views/Login.vue'),
         meta: {
+          guestOnly: true,
           title: '로그인',
           section: '로그인',
         },
@@ -123,8 +121,9 @@ const routes = [
         name: 'signup',
         component: () => import('@/views/Signup.vue'),
         meta: {
+          requiresAuth: true,
           title: '회원가입',
-          section: '회원가입',
+          section: '회원 생성',
         },
       },
     ],
