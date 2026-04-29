8f20979c6e3ec85e90d74533cd829dc47eebc4b1
Tahcy-99 <sjae999@gmail.com>
2026-04-29 17:19:26 +0900

    [Feat][B][MATCH_001] 계열사 자산 확인 및 등록
    등록 요청 생성 (AuthUserDetail 요소가 생성되지 않는 이슈 있음)

--- backend/src/main/java/org/example/backend/matching/controller/MatchingController.java
+++ backend/src/main/java/org/example/backend/matching/controller/MatchingController.java
@@ -5,9 +5,12 @@ import org.example.backend.common.model.BaseResponse;
 import org.example.backend.common.model.BaseResponseStatus;
 import org.example.backend.matching.model.MatchingDto;
 import org.example.backend.matching.service.AssetService;
+import org.example.backend.user.model.AuthUserDetails;
 import org.hibernate.JDBCException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,9 +26,8 @@ public class MatchingController {
             return ResponseEntity.ok(BaseResponse.success(dto));
         }
         catch (JDBCException e) {
-            return ResponseEntity.status(HttpStatus.OK)
-//                    .body(BaseResponse.success(e.getMessage()));
-                    .body(BaseResponse.fail(BaseResponseStatus.FAIL,e.getMessage()));
+            return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT)
+                    .body(BaseResponse.fail(BaseResponseStatus.FAIL,"데이터 없음"));
         }
     }
 
@@ -39,13 +41,21 @@ public class MatchingController {
             return ResponseEntity.ok(BaseResponse.success(dto));
         }
         catch (Exception e){
-            return ResponseEntity.status(HttpStatus.OK)
+            return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT)
                     .body(BaseResponse.fail(BaseResponseStatus.FAIL,e.getMessage()));
         }
     }
-//
-//    @PostMapping("/assets/add")
-//    public ResponseEntity addAsset(@RequestBody ){
-//
-//    }
+
+    @PostMapping("/asset/add")
+    public ResponseEntity addAsset(@RequestBody MatchingDto.AddAsset dto,
+                                   @AuthenticationPrincipal AuthUserDetails user){
+        try {
+            assetService.addAsset(dto, user);
+            return  ResponseEntity.ok(BaseResponse.success(BaseResponseStatus.ASSET_ADD_SUCCESS));
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT)
+                    .body(BaseResponse.fail(BaseResponseStatus.FAIL,e.getMessage()));
+        }
+    }
 }
