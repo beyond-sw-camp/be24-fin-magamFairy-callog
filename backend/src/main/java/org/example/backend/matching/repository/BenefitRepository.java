be32fb80436c581d1d8d69b7684002570cb060f9
Tahcy-99 <sjae999@gmail.com>
2026-05-08 14:40:05 +0900

    [Feat][B][EVALUATION] 혜택 목록을 캠페인 idx를 이용해 가져오도록 수정

--- backend/src/main/java/org/example/backend/matching/repository/BenefitRepository.java
+++ backend/src/main/java/org/example/backend/matching/repository/BenefitRepository.java
@@ -4,6 +4,9 @@ import org.example.backend.matching.model.PartnerBenefits;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BenefitRepository extends JpaRepository<PartnerBenefits,Long> {
+    List<PartnerBenefits> findAllByCampaignIdx(Long campaignIdx);
 }
