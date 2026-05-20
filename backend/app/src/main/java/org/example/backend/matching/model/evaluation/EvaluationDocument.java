c18b3898941b1ec763f02abf0f32eaf271d055da
Tahcy-99 <sjae999@gmail.com>
2026-05-21 08:57:57 +0900

    [Refactor][B][EVALUATEION] 평가 결과 조회 인덱싱 추가

--- backend/app/src/main/java/org/example/backend/matching/model/evaluation/EvaluationDocument.java
+++ backend/app/src/main/java/org/example/backend/matching/model/evaluation/EvaluationDocument.java
@@ -22,6 +22,7 @@ public class EvaluationDocument {
     @Indexed(unique=true)
     private String sessionId;
 
+    @Indexed
     private Long campaignIdx;  // 참조 관계는 조인 없이 ID 값만 유지합니다.
     private Long benefitIdx;
 
