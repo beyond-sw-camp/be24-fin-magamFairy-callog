102ec626f5f8493a93dbefec6190cc7cafcb86f0
Tahcy-99 <sjae999@gmail.com>
2026-05-21 15:54:33 +0900

    [Refactor][B][MSA] API-Gateway 적용

---
+++ backend/spring-gw/src/main/java/org/example/springgw/SpringGwApplication.java
@@ -0,0 +1,13 @@
+package org.example.springgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGwApplication.class, args);
+	}
+
+}
