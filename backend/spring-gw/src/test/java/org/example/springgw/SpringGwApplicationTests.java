1bb4b89d53bbf608824c9bad0e01438e73ec7f3f
Tahcy-99 <sjae999@gmail.com>
2026-05-22 12:13:46 +0900

    [Refactor][D][G][B][E][J][Infra] MSA 분리 배포

---
+++ backend/spring-gw/src/test/java/org/example/springgw/SpringGwApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
